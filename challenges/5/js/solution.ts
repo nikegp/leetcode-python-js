11b168de9ff1fd200ddbadad2dc23b28b7cb006e
Nike <nike.gurin@gmail.com>
2022-11-21 14:32:58 +0100

    Solve #5

--- challenges/5/js/solution.ts
+++ challenges/5/js/solution.ts
@@ -1,1 +1,45 @@
+import { expect } from "../../util";
 
+const longestPalindrome = (s: string): string => {
+  let left, longestPalindromeBoundaries, right;
+
+  if (s.length <= 1) {
+    return s;
+  }
+
+  longestPalindromeBoundaries = [0, 0];
+
+  for (let i = 0; i < s.length - 1; i += 1) {
+    [left, right] = getMaxPalindromeBoundaries(s, i, i);
+
+    if (right - left > longestPalindromeBoundaries[1] - longestPalindromeBoundaries[0]) {
+      longestPalindromeBoundaries = [left, right];
+    }
+
+    [left, right] = getMaxPalindromeBoundaries(s, i, i + 1);
+
+    if (right - left > longestPalindromeBoundaries[1] - longestPalindromeBoundaries[0]) {
+      longestPalindromeBoundaries = [left, right];
+    }
+  }
+
+  return s.slice(longestPalindromeBoundaries[0], longestPalindromeBoundaries[1] + 1);
+};
+
+const getMaxPalindromeBoundaries = (s: string, left = 0, right = 0): [number, number] => {
+  while (left >= 0 && right < s.length && s[left] === s[right]) {
+    left -= 1;
+    right += 1;
+  }
+
+  return [left + 1, right - 1];
+};
+
+expect(longestPalindrome("cbbd"), "bb");
+expect(longestPalindrome("bb"), "bb");
+expect(longestPalindrome("babad"), "bab");
+expect(longestPalindrome("cbbdjhf"), "bb");
+expect(longestPalindrome("aaavbn"), "aaa");
+expect(longestPalindrome("vbnaaa"), "aaa");
+expect(longestPalindrome("vbnaaaa"), "aaaa");
+expect(longestPalindrome("aaaavbn"), "aaaa");
--- challenges/5/python/solution.py
+++ challenges/5/python/solution.py
@@ -19,42 +19,34 @@ class Solution:
 
         return s[longest_palindrome_boundaries[0] : longest_palindrome_boundaries[1] + 1]
 
-    def get_max_palindrome_boundaries(self, s: str, left: int, right: int) -> (int, int):
-        iterations = 0
-        while left >= 0 and right < len(s) - 1:
-            if s[left] != s[right]:
-                break
-
+    def get_max_palindrome_boundaries(self, s: str, left: int = 0, right: int = 0) -> (int, int):
+        while left >= 0 and right < len(s) and s[left] == s[right]:
             left -= 1
             right += 1
-            iterations += 1
 
-        if iterations:
-            if (left < 0 or right >= len(s)) or (s[left] != s[right]):
-                return left + 1, right - 1
-            else:
-                return left, right
-        else:
-            return left, right
+        return left + 1, right - 1
 
 
 if __name__ == "__main__":
     obj = Solution()
 
-    expect(obj.get_max_palindrome_boundaries("bb", 0, 0), (0, 0))
-    expect(obj.get_max_palindrome_boundaries("bb", 0, 1), (0, 1))
-    expect(obj.get_max_palindrome_boundaries("bbbad", 0, 0), (0, 0))
-    expect(obj.get_max_palindrome_boundaries("bbad", 0, 1), (0, 1))
-    expect(obj.get_max_palindrome_boundaries("bbbad", 1, 1), (0, 2))
-    expect(obj.get_max_palindrome_boundaries("asabba", 3, 4), (2, 5))
-    expect(obj.get_max_palindrome_boundaries("asabba", 3, 3), (3, 3))
-    expect(obj.get_max_palindrome_boundaries("asaba", 3, 3), (2, 4))
-    expect(obj.get_max_palindrome_boundaries("asaaaba", 3, 3), (2, 4))
+    # expect(obj.get_max_palindrome_boundaries("bb", 0, 0), (0, 0))
+    # expect(obj.get_max_palindrome_boundaries("bb", 0, 1), (0, 1))
+    # expect(obj.get_max_palindrome_boundaries("cbbd", 1, 1), (1, 1))
+    # expect(obj.get_max_palindrome_boundaries("cbbd", 1, 2), (1, 2))
+    # expect(obj.get_max_palindrome_boundaries("bbbad", 0, 0), (0, 0))
+    # expect(obj.get_max_palindrome_boundaries("bbad", 0, 1), (0, 1))
+    # expect(obj.get_max_palindrome_boundaries("bbbad", 1, 1), (0, 2))
+    # expect(obj.get_max_palindrome_boundaries("asabba", 3, 4), (2, 5))
+    # expect(obj.get_max_palindrome_boundaries("asabba", 3, 3), (3, 3))
+    # expect(obj.get_max_palindrome_boundaries("asaba", 3, 3), (2, 4))
+    # expect(obj.get_max_palindrome_boundaries("asaaaba", 3, 3), (2, 4))
 
-    # expect(obj.longestPalindrome("bb"), "bb")
-    # expect(obj.longestPalindrome("babad"), "bab")
-    # expect(obj.longestPalindrome("cbbdjhf"), "bb")
-    # expect(obj.longestPalindrome("aaavbn"), "aaa")
-    # expect(obj.longestPalindrome("vbnaaa"), "aaa")
-    # expect(obj.longestPalindrome("vbnaaaa"), "aaaa")
-    # expect(obj.longestPalindrome("aaaavbn"), "aaaa")
+    expect(obj.longestPalindrome("cbbd"), "bb")
+    expect(obj.longestPalindrome("bb"), "bb")
+    expect(obj.longestPalindrome("babad"), "bab")
+    expect(obj.longestPalindrome("cbbdjhf"), "bb")
+    expect(obj.longestPalindrome("aaavbn"), "aaa")
+    expect(obj.longestPalindrome("vbnaaa"), "aaa")
+    expect(obj.longestPalindrome("vbnaaaa"), "aaaa")
+    expect(obj.longestPalindrome("aaaavbn"), "aaaa")
